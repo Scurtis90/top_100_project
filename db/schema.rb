24e159705d590c6649401fabd8e42f850fcf8f2f
Spencer Curtis <spencerscurtis@gmail.com>
2021-03-07 17:09:39 -0700

    added song model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_07_235226) do
+ActiveRecord::Schema.define(version: 2021_03_08_000443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,16 @@ ActiveRecord::Schema.define(version: 2021_03_07_235226) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.string "album"
+    t.string "genre"
+    t.bigint "artist_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
+
   add_foreign_key "artists", "billboards"
+  add_foreign_key "songs", "artists"
 end
