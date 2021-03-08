9e332f61cab0b29fb827d91f766865ffbc3c6298
LuLu DeHaan <luludehaan@gmail.com>
2021-03-07 19:15:27 -0800

    billboards controller

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,13 +1,50 @@
 class BillboardsController < ApplicationController
   def index
+    @billboards = Billboard.all
+    render component: 'Billboards', props: { billboards: @billboards }
   end
 
   def show
+    @billboards = Billboard.find(params[:id])
+    render component: 'Billboard', props: { billboard: @billboard }
   end
 
   def new
+    @billboard = Billboard.new
+    render component: 'BillboardNew', props: { billboard: @billboard }
+  end
+
+  def create
+    @billboard = Billboard.new(billboard_params)
+    if billboard.save
+      redirect_to billboards_path
+    else
+      render component: 'BillboardNew', props: { billboard: @billboard }
+    end
   end
 
   def edit
+    @billboard = Billboard.find(params[:id])
+    render component: 'BillboardEdit', props: { billboard: @billboard }
+  end
+
+  def update
+    @billboard = Billboards.find(params{:id})
+    if @billboard.update(billboard_params)
+      redirect_to billboards_path
+    else
+      render component: 'BillboardEdit', props: { billboard: @billboard }
+    end
+  end
+
+  def destroy
+    @billboard = Billboard.find(params[:id])
+    @billboard.destroy
+    redirect_to billboards_path
+  end
+
+  private
+  def billboard_params
+    params.require(:billboard).permit(:title, :details)
   end
 end
