cf86f3c88dee17d31d6b1354de8587becd2273cb
Spencer Curtis <spencerscurtis@gmail.com>
2021-03-07 17:20:56 -0700

    added validations

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,4 +1,6 @@
 class Artist < ApplicationRecord
   belongs_to :billboard
   has_many :songs dependent: :destroy
+  validates :name, :rank, presence: true
+  validates :name, :rank, uniqueness: true
 end
