85e4109de919f4916fb232e320e34af17f802a37
Spencer Curtis <spencerscurtis@gmail.com>
2021-03-11 16:59:22 -0700

    added comments into seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+#couldnt get this to work. not sure what im missing.
+
+# 3.times do
+#   billboard = Billbaord.create(
+#     title: Faker::Beer.brand
+#   )
+
+#   3.times do
+#     artist = Artist.create(
+#       name: Faker::Name.middle_name,
+#       rank: rand(1...20),
+#       billboard_id: billboard.id
+#     )
+
+#     3.times do
+#       song = Song.create(
+#         title: Faker::Hipster.words
+#         genre: Faker::Hipster.words
+#         album: Faker::Hipster.sentence
+#         artist_id: artist.id
+#       )
+#     end
+#   end
+# end
+
+# puts "Data Seeded"
